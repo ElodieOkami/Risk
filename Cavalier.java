b7b20178bb5cba91353455d36608f940bd1594d5
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-05-26 18:44:44 +0200

    Add files via upload

---
+++ Cavalier.java
@@ -0,0 +1,10 @@
+
+public class Cavalier extends Unite{
+
+	public Cavalier(int cout, int puissance, int prioriteATT, int prioriteDEF, int mvtTour, int token,
+			Joueur[] listeJoueur) {
+		super(cout, puissance, prioriteATT, prioriteDEF, mvtTour, token, listeJoueur);
+		// TODO Auto-generated constructor stub
+	}
+
+}
