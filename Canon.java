308ab528cba69957d13913ddceae7fadee182e46
Aenerya <33449933+Aenerya@users.noreply.github.com>
2018-05-29 22:42:20 +0200

    Update Canon.java

--- Canon.java
+++ Canon.java
@@ -1,9 +1,7 @@
-
 public class Canon extends Unite {
 
-	public Canon(int cout, int puissance, int prioriteATT, int prioriteDEF, int mvtTour, int token,
-			Joueur[] listeJoueur) {
-		super(cout, puissance, prioriteATT, prioriteDEF, mvtTour, token, listeJoueur);
+	public Canon(int cout, int[] puissance, int prioriteATT, int prioriteDEF, int mvtTour, String token) {
+		super(cout, puissance, prioriteATT, prioriteDEF, mvtTour, token);
 		// TODO Auto-generated constructor stub
 	}
 
