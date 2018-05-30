d126f45d9ce00b798f16cdc0de9a00889e1994e2
Aenerya <33449933+Aenerya@users.noreply.github.com>
2018-05-30 21:29:31 +0200

    Update Unite.java

--- Unite.java
+++ Unite.java
@@ -57,44 +57,48 @@ public class Unite {
 
 //Fonctions
 
-	public void repartitionUnite()
+	public static void repartitionUnite(int nbrJr)
 	{
-		int nbrJoueur = RiskIsep.MenuJoueur(); // nombre de joueurs sur la partie actuelle
 		int[] listePuissanceSoldat = {1,2,3,4,5,6};
-		switch(nbrJoueur) //On gère les différents cas
+		switch(nbrJr) //On gère les différents cas
 		{
-		case '1' :
-			
+		case 1 :
+			System.out.println("1 joueur");
 			break;
-		case '2' :
+		case 2 :
 			for(int i = 0; i < 40; i++)
 			{
 				Joueur.listeUnite.add(new Soldat(1, listePuissanceSoldat, 2, 1, 2, "Soldat"));
 			}
+			System.out.println("Les deux joueurs ont reçu 40 soldats");
 			break;
-		case '3' :
+		case 3 :
 			for(int i = 0; i < 35; i++)
 			{
 				Joueur.listeUnite.add(new Soldat(1, listePuissanceSoldat, 2, 1, 2, "Soldat"));
 			}
+			System.out.println("Les trois joueurs ont reçu 35 soldats");
 			break;
-		case '4' :
+		case 4 :
 			for(int i = 0; i < 30; i++)
 			{
 				Joueur.listeUnite.add(new Soldat(1, listePuissanceSoldat, 2, 1, 2, "Soldat"));
 			}
+			System.out.println("Les quatre joueurs ont reçu 30 soldats");
 			break;
-		case '5' :
+		case 5 :
 			for(int i = 0; i < 25; i++)
 			{
 				Joueur.listeUnite.add(new Soldat(1, listePuissanceSoldat, 2, 1, 2, "Soldat"));
 			}
+			System.out.println("Les cinq joueurs ont reçu 25 soldats");
 			break;
-		case '6' :
+		case 6 :
 			for(int i = 0; i < 20; i++)
 			{
 				Joueur.listeUnite.add(new Soldat(1, listePuissanceSoldat, 2, 1, 2, "Soldat"));
 			}
+			System.out.println("Les six joueurs ont reçu 20 soldats");
 			break;
 		}
 	}
