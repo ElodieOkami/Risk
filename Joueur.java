3ae5d80a924c76617ebff5ddd1174f1969aa09bf
ElodieOkami <33449924+ElodieOkami@users.noreply.github.com>
2018-05-31 17:13:41 +0200

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,30 +1,31 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.util.ArrayList;
-import java.util.Random;
-
-
 
 public class Joueur {
 	int idJoueur;
 	static ArrayList<Unite> listeUnite = new ArrayList <Unite>();
 	String pseudo;
 	Color couleur;
-	int idMission;
 	
 	//----------------------    Constructeur     ---------------------------//
 	
-	public Joueur(int idJoueur, ArrayList<Unite> listeUnite, String pseudo, Color couleur,int idMission) {
+	public Joueur(int idJoueur, ArrayList<Unite> listeUnite, String pseudo, Color couleur) {
 		super();
 		this.idJoueur = idJoueur;
 		this.listeUnite = listeUnite;
 		this.pseudo = pseudo;
 		this.couleur = couleur;
-		this.idMission = idMission;
 	}
 
 	
 	//----------------------    Getters et Setters     ---------------------------//
+	public int getIdJoueur() {
+		return idJoueur;
+	}
+	public void setIdJoueur(int idJoueur) {
+		this.idJoueur = idJoueur;
+	}
 	public ArrayList<Unite> getListeUnite() {
 		return listeUnite;
 	}
@@ -43,90 +44,60 @@ public class Joueur {
 	public void setCouleur(Color couleur) {
 		this.couleur = couleur;
 	}
-	public int getIdMission() {
-		return idMission;
-	}
-	public void setIdMission(int idMission) {
-		this.idMission = idMission;
-	}
-	public int getIdJoueur() {
-		return idJoueur;
-	}
-	public void setIdJoueur(int idJoueur) {
-		this.idJoueur = idJoueur;
-	}
-	
-	
-
-	//----------------------    Création des joueurs     ---------------------------//
-	static ArrayList<Unite> liste = new ArrayList();
-	static Joueur joueur1 = new Joueur(0, liste , "joueur1", Color.BLUE,0);
-	static Joueur joueur2 = new Joueur(0, liste , "joueur2", Color.RED,0);
-	static Joueur joueur3 = new Joueur(0, liste , "joueur3", Color.GREEN,0);
-	static Joueur joueur4 = new Joueur(0, liste , "joueur4", Color.PINK,0);
-	static Joueur joueur5 = new Joueur(0, liste , "joueur5", Color.YELLOW,0);
-	static Joueur joueur6 = new Joueur(0, liste , "joueur6", Color.BLACK,0);
 	
 	
 	//----------------------    Fonctions     ---------------------------//
+	public static void creaJoueur(int nbrJr)
+	{
+		Color[] list = { Color.BLUE, Color.RED, Color.GREEN, Color.PINK, Color.YELLOW, Color.BLACK};
+		
+		for (int i = 0; i <= nbrJr-1 ; i++)
+		{
+			Joueur joueur = new Joueur (i+1, listeUnite ,"joueur" + i+1, list[i]);
+		}
+	}
+	
 	public static int nombreJoueurs(double clickX, double clickY)
 	{
 		Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 		int height = (int)dimension.getHeight();
 		int width  = (int)dimension.getWidth();
-		
-			Unite[] liste = {};
-			
+					
 			if(height/1.5-20 < clickY && clickY < height/1.5+20) 
 			{
 				if (width/3.9-15 < clickX && clickX < width/3.9+15) 
 				{
-					joueur1.idJoueur = 1;
+					creaJoueur(1);
 					return 1;
 				}
 				
 				else if (width/2.8-15 < clickX && clickX < width/2.8+15) 
 				{
-					joueur1.idJoueur = 1;
-					joueur2.idJoueur = 2;
+					creaJoueur(2);
 					return 2;
 				}
 				
 				else if (width/2.2-15 < clickX && clickX < width/2.2+15)
 				{
-					joueur1.idJoueur = 1;
-					joueur2.idJoueur = 2;
-					joueur3.idJoueur = 3;
+					creaJoueur(3);
 					return 3;
 				}
 				
 				else if (width/1.8-15 < clickX && clickX < width/1.8+15)
 				{
-					joueur1.idJoueur = 1;
-					joueur2.idJoueur = 2;
-					joueur3.idJoueur = 3;
-					joueur4.idJoueur = 4;
+					creaJoueur(4);
 					return 4;
 				}
 				
 				else if (width/1.55-15 < clickX && clickX < width/1.55+15)
 				{
-					joueur1.idJoueur = 1;
-					joueur2.idJoueur = 2;
-					joueur3.idJoueur = 3;
-					joueur4.idJoueur = 4;
-					joueur5.idJoueur = 5;
+					creaJoueur(5);
 					return 5;
 				}
 				
 				else if (width/1.35-15 < clickX && clickX < width/1.35+15)
 				{
-					joueur1.idJoueur = 1;
-					joueur2.idJoueur = 2;
-					joueur3.idJoueur = 3;
-					joueur4.idJoueur = 4;
-					joueur5.idJoueur = 5;
-					joueur6.idJoueur = 6;
+					creaJoueur(6);
 					return 6;
 				}
 			}
