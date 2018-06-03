5a236228a6a18dee6cf0e93bc84f91651bbee03b
ElodieOkami <33449924+ElodieOkami@users.noreply.github.com>
2018-06-03 13:29:40 +0200

    Update Mission.java

--- Mission.java
+++ Mission.java
@@ -1,14 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Mission {
 	String contenu;
 	int idMission;
+	int proprietaire;
 	
 	
 	//----------------------    Constructeur     ---------------------------//
-	public Mission(String contenu,int idMission) {
+	public Mission(String contenu,int idMission,int proprietaire) {
 		super();
 		this.contenu = contenu;
 		this.idMission = idMission;
+		this.proprietaire = proprietaire; 
 	}
 
 	//----------------------    Getters et Setters     ---------------------------//
@@ -18,5 +23,18 @@ public class Mission {
 	public void setContenu(String contenu) {
 		this.contenu = contenu;
 	}
-
+	public int getIdMission() {
+		return idMission;
+	}
+	public void setIdMission(int idMission) {
+		this.idMission = idMission;
+	}
+	public int getProprietaire() {
+		return proprietaire;
+	}
+	public void setProprietaire(int proprietaire) {
+		this.proprietaire = proprietaire;
+	}
+	
+	
 }
