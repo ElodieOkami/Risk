32ae3317a011b023a442cbf7f7d097bf88106a2f
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-06-04 23:46:08 +0200

    Update Canon.java

--- Canon.java
+++ Canon.java
@@ -1,7 +1,7 @@
 public class Canon extends Unite {
 
-	public Canon(int cout, int[] puissance, int prioriteATT, int prioriteDEF, int mvtTour, String token) {
-		super(cout, puissance, prioriteATT, prioriteDEF, mvtTour, token);
+	public Canon(int cout, int[] puissance, int prioriteATT, int prioriteDEF, int mvtTour, int idPosition, String token) {
+		super(cout, puissance, prioriteATT, prioriteDEF, mvtTour, idPosition, token);
 		// TODO Auto-generated constructor stub
 	}
 
