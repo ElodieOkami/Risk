341dad4debf3ddf4ad0bf970ec76df4cbc1fe289
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-06-04 23:46:22 +0200

    Update Cavalier.java

--- Cavalier.java
+++ Cavalier.java
@@ -1,8 +1,7 @@
-
 public class Cavalier extends Unite{
 
-	public Cavalier(int cout, int[] puissance, int prioriteATT, int prioriteDEF, int mvtTour, String token) {
-		super(cout, puissance, prioriteATT, prioriteDEF, mvtTour, token);
+	public Cavalier(int cout, int[] puissance, int prioriteATT, int prioriteDEF, int mvtTour, int idPosition, String token) {
+		super(cout, puissance, prioriteATT, prioriteDEF, mvtTour, idPosition, token);
 		// TODO Auto-generated constructor stub
 	}
 
