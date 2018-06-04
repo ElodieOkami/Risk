ed166ca1b1cd28ac9c0f914aa8692dcc0e761855
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-06-04 15:46:44 +0200

    Update RiskIsep.java

--- RiskIsep.java
+++ RiskIsep.java
@@ -3,17 +3,19 @@ public class RiskIsep {
 
 	public static void main(String[] args) {
 		Jeu();
-		
+		//new Panel();
 	}
 	
 	public static void Jeu()
 	{
 		String cartePng = Initialisation.initialisationJeu();
 		
+		
+		
 		while(true)
 		{
-		Interface.lectureClic(cartePng);
-		
+			//Region.appelAfficheProprio(cartePng);
+			Interface.lectureClic(cartePng);
 		}
 		//Tour.Tour(); (pour la future class tour)
 	}
