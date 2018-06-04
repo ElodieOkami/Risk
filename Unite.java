a2a2473ed18055f811127955af40235ab2364f45
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-06-04 23:50:21 +0200

    Update Unite.java

--- Unite.java
+++ Unite.java
@@ -4,16 +4,18 @@ public class Unite {
 	int prioriteATT;
 	int prioriteDEF;
 	int mvtTour;
+	int idPosition;
 	String token;
 	
 	//Constructeurs
-	public Unite(int cout, int[] puissance, int prioriteATT, int prioriteDEF, int mvtTour, String token) {
+	public Unite(int cout, int[] puissance, int prioriteATT, int prioriteDEF, int mvtTour, int idPosition, String token) {
 		super();
 		this.cout = cout;
 		this.puissance = puissance;
 		this.prioriteATT = prioriteATT;
 		this.prioriteDEF = prioriteDEF;
 		this.mvtTour = mvtTour;
+		this.idPosition = idPosition;
 		this.token = token;
 	}
 
@@ -47,6 +49,12 @@ public class Unite {
 	}
 	public void setMvtTour(int mvtTour) {
 		this.mvtTour = mvtTour;
+	}
+	public int getIdPosition() {
+		return idPosition;
+	}
+	public void setIdPosition(int idPosition) {
+		this.idPosition = idPosition;
 	}
 	public String getToken() {
 		return token;
