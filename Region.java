0e98db7b4c421040b5105bc89caf508f839c14b0
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-06-04 23:48:43 +0200

    Update Region.java

--- Region.java
+++ Region.java
@@ -43,7 +43,7 @@ public class Region {
 		this.listeTerritoires.add(territoire);
 	}
 		
-		public void creaEtAttribTerritoires(int idTerritoire, int nbrJr, ArrayList <Integer> Joueur, ArrayList <Integer> JrFull)		//Création des territoires de chaque région et répartion aléatoire parmi les joueurs
+		public void creaEtAttribTerritoires(int idTerritoire, int nbrJr, ArrayList <Integer> listIdJoueur, ArrayList <Integer> JrFull)		//Création des territoires de chaque région et répartion aléatoire parmi les joueurs
 		{
 			int terParJr=(int)(42/nbrJr);		//Nombre de territoire arrondi que se verra attribué chaque joueur
 			
@@ -54,22 +54,22 @@ public class Region {
 				this.ajouterTerritoires(i, idTerritoire);		//On ajoute le territoire dans l'arraylist territoires de cette région
 				idTerritoire++;							//Le prochain territoire possède l'id suivant
 				
-				int j = (int)(Math.random()*(nbrJr-(nbrJr-Joueur.size()))+1);	//Nombre aléatoire entre 1 et le nbr de joueur pas encore full	
-				this.listeTerritoires.get(i-1).setProprietaire(Joueur.get(j-1));		//On attribue le territoire à un joueur
+				int j = (int)(Math.random()*(nbrJr-(nbrJr-listIdJoueur.size()))+1);	//Nombre aléatoire entre 1 et le nbr de joueur pas encore full	
+				this.listeTerritoires.get(i-1).setProprietaire(listIdJoueur.get(j-1));		//On attribue le territoire à un joueur
 				
 				//System.out.println(this.getNom()+Territoire.getNumero() + " Id : " +this.listeTerritoires.get(i-1).getId() + " et Propri : " +this.listeTerritoires.get(i-1).getProprietaire());
 				
 				JrFull.set(j-1, (1+JrFull.get(j-1)));	//On augmente la quantité de territoire que possède le joueur
 				if (JrFull.get(j-1)==terParJr)			//Si le joueur possède le nombre max de territoires
 				{
-					Joueur.remove(j-1);					//On supprime ce joueur des candidats
+					listIdJoueur.remove(j-1);					//On supprime ce joueur des candidats
 					JrFull.remove(j-1);
-					if (Joueur.isEmpty())				//Si on a plus aucun candidat
+					if (listIdJoueur.isEmpty())				//Si on a plus aucun candidat
 					{
 						
 						for (int z=0; z<nbrJr; z++)		//Jusqu'à ce qu'on atteigne le nombre total de joueurs
 						{
-							Joueur.add(z, z+1);			//On recréé chaque candidat
+							listIdJoueur.add(z, z+1);			//On recréé chaque candidat
 							JrFull.add(z, terParJr-1); 	//On recréé un d'espace pour chaque candidat
 						}
 					}
@@ -85,17 +85,18 @@ public class Region {
 		{
 			int numPropri;
 			
-			for (int i=0; i<this.getTaille(); i++)
+			for (int i=0; i<this.getTaille(); i++)	//Pour chaque territoire de la Région
 			{
-				numPropri = this.listeTerritoires.get(i).getProprietaire();
-				Color couleur = Initialisation.getCouleurPropri(numPropri);
-				Plateau.affichePointProprio(idTerr, couleur);
+				numPropri = this.listeTerritoires.get(i).getProprietaire();		//Ressort le proprietaire de chaque territoire de la Région
+				Color couleur = Initialisation.getCouleurPropri(numPropri);		//Ressort la couleur de ce propriétaire
+				Plateau.affichePointProprio(idTerr, couleur);					//Affiche un point de la couleur du joueur sur le territoire qu'il occupe 
 				idTerr++;
 			}
 			return idTerr;
 			
 			
 		}
+		
 		
 	
 	
