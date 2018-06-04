6a2b1db6ab40733ca564faf2e174eb583d2ea723
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-06-04 23:49:45 +0200

    Update Territoire.java

--- Territoire.java
+++ Territoire.java
@@ -2,6 +2,9 @@ public class Territoire {
 	public static int numero;			//Numéro dans la région
 	public int proprietaire;	//Propriétaire de ce territoire
 	public int id;		//id parmi les 42 du plateau
+	public int nbrCanon;
+	public int nbrCaval;
+	public int nbrSoldat;
 	
 	
 	//----------------------    Constructeur     ---------------------------//
@@ -32,6 +35,24 @@ public class Territoire {
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	public int getNbrCanon() {
+		return nbrCanon;
+	}
+	public void setNbrCanon(int nbrCanon) {
+		this.nbrCanon = nbrCanon;
+	}
+	public int getNbrCaval() {
+		return nbrCaval;
+	}
+	public void setNbrCaval(int nbrCaval) {
+		this.nbrCaval = nbrCaval;
+	}
+	public int getNbrSoldat() {
+		return nbrSoldat;
+	}
+	public void setNbrSoldat(int nbrSoldat) {
+		this.nbrSoldat = nbrSoldat;
 	}
 	
 	
