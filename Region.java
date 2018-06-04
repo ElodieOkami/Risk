edfab7f8f471c4d24999ed65630e1ad8fdadf3ca
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-06-04 15:46:25 +0200

    Update Region.java

--- Region.java
+++ Region.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.util.ArrayList;
 
 public class Region {
@@ -35,90 +36,14 @@ public class Region {
 	public void setTerritoires(ArrayList<Territoire> listeTerritoires) {
 		this.listeTerritoires = listeTerritoires;
 	}
-		
-	
-	
-	public static void CreaRegTer(String cartePng, int nbrJr)
-	{
-		Plateau.affichePlateau(cartePng);					//Affiche l'image du plateau de jeu
-		int idTerritoire=0;
-		creationRegions(cartePng, nbrJr, idTerritoire);			//Crée les Régions ainsi que les territoires
-	}
-	
-	
-	public static int creationRegions(String cartePng, int nbrJr, int idTerritoire)			//Crée les Régions ainsi que les territoires
-	{
-		
-		ArrayList <Integer> JrFull = new ArrayList<Integer>();				//On créé une liste du nombre de territoire qu'a chaque joueur lors de l'attribution aléatoire
-		for (int i=0; i<nbrJr; i++)
-		{
-			JrFull.add(0);													//Cette liste est initialement vide
-		}
-		
-		ArrayList <Integer> Joueur = new ArrayList<Integer>();				//On créé une liste de "l'id" de chaque joueur
-		for (int i=0; i<nbrJr; i++)
-		{
-			Joueur.add(i+1);													//Cette liste va de 1 au nombre de joueur
-		}
-		//System.out.println("Nombre de joueurs: " + Joueur.get(nbrJr-1));
-		
-		if (cartePng == "plateauElder.png")		//Si on utilise la carte TES
-		{
-			
-			Region provincesImperiales = new Region("provincesImperiales", 11);			//On créé la région a l'aide du constructeur
-			provincesImperiales.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);	//On créé chaque territoire des régions et on les répartis aléatoirement parmi les joueurs.
-			idTerritoire += provincesImperiales.getTaille()-1;
-			Region lenclume = new Region("lenclume", 5);
-			lenclume.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += lenclume.getTaille();
-			Region cyrodiil = new Region("cyrodiil", 8);
-			cyrodiil.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += cyrodiil.getTaille();
-			Region archipelAutomne = new Region("archipelAutomne", 3);
-			archipelAutomne.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += archipelAutomne.getTaille();
-			Region aldmeri = new Region("aldmeri", 5);
-			aldmeri.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += aldmeri.getTaille();
-			Region argonie = new Region("argonie", 10);
-			argonie.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += argonie.getTaille();
-			System.out.println(idTerritoire);
-		}
-		
-		else if (cartePng == "plateauTerre.png")
-		{
-			Region northAmerica = new Region("northAmerica",9);
-			northAmerica.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += northAmerica.getTaille();
-			Region southAmerica = new Region("southAmerica",4);
-			southAmerica.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += southAmerica.getTaille();
-			Region europe = new Region("europe",7);
-			europe.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += europe.getTaille();
-			Region africa = new Region("africa",6);
-			africa.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += africa.getTaille();
-			Region asia = new Region("asia",12);
-			asia.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += asia.getTaille();
-			Region australia = new Region("australia",4);
-			australia.creaEtAttribTerritoires(idTerritoire, nbrJr, Joueur, JrFull);
-			idTerritoire += australia.getTaille();
-			
-		}
-		return idTerritoire;
-	}
 	
 	private void ajouterTerritoires(int numero, int id)			//Ajoute un territoire dans l'arryalist territoires
 	{
 		Territoire territoire = new Territoire(numero, id);
 		this.listeTerritoires.add(territoire);
 	}
-	
-	
-		private void creaEtAttribTerritoires(int idTerritoire, int nbrJr, ArrayList <Integer> Joueur, ArrayList <Integer> JrFull)		//Création des territoires de chaque région et répartion aléatoire parmi les joueurs
+		
+		public void creaEtAttribTerritoires(int idTerritoire, int nbrJr, ArrayList <Integer> Joueur, ArrayList <Integer> JrFull)		//Création des territoires de chaque région et répartion aléatoire parmi les joueurs
 		{
 			int terParJr=(int)(42/nbrJr);		//Nombre de territoire arrondi que se verra attribué chaque joueur
 			
@@ -155,6 +80,22 @@ public class Region {
 			
 		}
 		
+		
+		public void getPossesseurDsRegion()
+		{
+			int numPropri;
+			int idTerr = 0;
+			for (int i=0; i<this.getTaille(); i++)
+			{
+				numPropri = this.listeTerritoires.get(idTerr).getProprietaire();
+				Color couleur = Initialisation.getCouleurPropri(numPropri);
+				Plateau.affichePointProprio(idTerr, couleur);
+				idTerr++;
+			}
+			
+			
+			
+		}
 		
 	
 	
