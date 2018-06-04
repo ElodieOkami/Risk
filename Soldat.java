0033d11b32ed5ac74a0a838a6bf33e545fbfe74e
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-06-04 23:49:23 +0200

    Update Soldat.java

--- Soldat.java
+++ Soldat.java
@@ -1,7 +1,7 @@
 public class Soldat extends Unite {
 
-	public Soldat(int cout, int[] puissance, int prioriteATT, int prioriteDEF, int mvtTour, String token) {
-		super(cout, puissance, prioriteATT, prioriteDEF, mvtTour, token);
+	public Soldat(int cout, int[] puissance, int prioriteATT, int prioriteDEF, int mvtTour, int idPosition, String token) {
+		super(cout, puissance, prioriteATT, prioriteDEF, mvtTour, idPosition, token);
 		// TODO Auto-generated constructor stub
 	}
 }
