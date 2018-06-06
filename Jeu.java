e081edb671f617e3227c3cd1b7cbcc6e95d7f256
ElodieOkami <33449924+ElodieOkami@users.noreply.github.com>
2018-06-06 15:00:37 +0200

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -1,13 +1,23 @@
 
+
 public class Jeu {
 	
 	
-	public static int partieDeRisk(int nbrJr) //Fonction appellée par le Main
+	public static int partieDeRisk(int nbrJr, String cartePng) //Fonction appellée par le Main
 	{
 		int joueurGagnant =-1;
 		while(joueurGagnant ==-1)
 		{
-			
+			for (int i=0; i<nbrJr; i++)
+			{
+				Plateau.affichePlateau(cartePng);
+				RiskIsep.PossesseurTerris(cartePng);
+				while(Interface.isFinDeTourPressed(i) == false)
+				{
+					Tour.tour(i);
+				}
+				
+			}
 			
 			
 			
