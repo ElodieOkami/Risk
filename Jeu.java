4435fc68d0e58798a4f2d6b910612cdba1c537dd
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-06-06 22:03:17 +0200

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -2,12 +2,16 @@
 public class Jeu {
 	
 	
-	public static int partieDeRisk(int nbrJr) //Fonction appellée par le Main
+	public static int partieDeRisk(int nbrJr, String cartePng) //Fonction appellée par le Main
 	{
 		int joueurGagnant =-1;
 		while(joueurGagnant ==-1)
 		{
-			
+			for (int i=0; i<nbrJr; i++)
+			{
+				Tour.tour(i, cartePng);
+			}
+				
 			
 			
 			
