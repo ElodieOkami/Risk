8ad9a7a0b1977ade4cc63309f32caf47126f9ab5
EcuLouis <33450030+EcuLouis@users.noreply.github.com>
2018-06-07 21:34:58 +0200

    Update Tour.java

--- Tour.java
+++ Tour.java
@@ -4,28 +4,42 @@
 		
 	public static void tour(int idJoueur, String cartePng)
 	{
+		boolean tourPasFini = true;
+		
 			int renforts;
 			renforts = RiskIsep.appelFonctionRenforts(idJoueur);
 			RiskIsep.choixUniteEtPlacerRenforts(renforts, idJoueur, cartePng);
+			String deplAttqTerm="undefined";
 			
-			String deplAttqTerm = "deplacement";	//Mettre la	 fonction de Maya sur le choix du menu
-			switch(deplAttqTerm)
+			while(tourPasFini)
 			{
-			case "deplacement" :
-				Territoire.deplacement(idJoueur, cartePng); //Fonction déplacement
+				while (deplAttqTerm == "undefined")
+				{
+					deplAttqTerm = Interface.whichButtonIsPressed();	//Mettre la	 fonction de Maya sur le choix du menu
+					//On fait rien, on attend juste pour passer à la suite
+				}
 				
-				break;
+				switch(deplAttqTerm)
+				{
+				case "deplacement" :
+					Plateau.afficheInfosSeDeplacer(cartePng);
+					//Territoire.deplacement(idJoueur, cartePng); //Fonction déplacement
+					deplAttqTerm = "undefined";
+					break;
+				
+				case "attaque":
+									//Fonction attaque
+					deplAttqTerm = "undefined";
+					break;
+					
+				case "terminer":
+									//Fonction fin de tour (juste break?)
+					tourPasFini = false;
+					break;
+				}
+		}
+		
 			
-			case "attaque":
-								//Fonction attaque
-				
-				break;
-				
-			case "terminer":
-								//Fonction fin de tour (juste break?)
-				
-				break;
-			}
 	}  
   
 	
@@ -38,6 +52,49 @@
 		
 	}
 	
+
+	public static int nombreSoldatJoueur(Joueur joueur) //renvoie le nombre de soldats d'un joueur
+		{
+			int compteur = 0;
+			int nbrUnites = joueur.getListeUnite().size();
+			for(int i = 0; i < nbrUnites; i++)
+			{
+				if(joueur.getListeUnite().get(i).type == "soldat")
+					{
+						compteur = compteur + 1;
+					}
+			}
+			return compteur;
+		}
+		
+		public static int nombreCavalierJoueur(Joueur joueur) //renvoie le nombre de cavaliers d'un joueur
+		{
+			int compteur = 0;
+			int nbrUnites = joueur.getListeUnite().size();
+			for(int i = 0; i < nbrUnites; i++)
+			{
+				if(joueur.getListeUnite().get(i).type == "cavalier")
+					{
+						compteur = compteur + 1;
+					}
+			}
+			return compteur;
+		}
+		
+		public static int nombreCanonJoueur(Joueur joueur) //renvoie le nombre de canons d'un joueur
+		{
+			int compteur = 0;
+			int nbrUnites = joueur.getListeUnite().size();
+			for(int i = 0; i < nbrUnites; i++)
+			{
+				if(joueur.getListeUnite().get(i).type == "canon")
+					{
+						compteur = compteur + 1;
+					}
+			}
+			return compteur;
+		}
+		
 	
 
 }
