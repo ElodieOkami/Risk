ddd417b1c4fe347ac8ac629e1b31b7ac3c51f67b
Aenerya <33449933+Aenerya@users.noreply.github.com>
2018-06-07 19:04:31 +0200

    Update Territoire.java

--- Territoire.java
+++ Territoire.java
@@ -1,7 +1,11 @@
 public class Territoire {
-	static int numero;			//Numéro dans la région
-	static int proprietaire;	//Propriétaire de ce territoire
-	static int id;		//id parmi les 42 du plateau
+	public static int numero;			//Numéro dans la région
+	public int proprietaire;	//Propriétaire de ce territoire
+	public int id;		//id parmi les 42 du plateau
+	public int nbrCanon;
+	public int nbrCaval;
+	public int nbrSoldat;
+	
 	
 	
 	//----------------------    Constructeur     ---------------------------//
@@ -9,7 +13,7 @@ public class Territoire {
 	public Territoire(int numero, int id) {
 		super();
 		Territoire.numero = numero;
-		Territoire.id = id;
+		this.id = id;
 	}
 
 	
@@ -21,11 +25,11 @@ public class Territoire {
 	public void setNumero(int numero) {
 		Territoire.numero = numero;
 	}
-	public static int getProprietaire() {
+	public int getProprietaire() {
 		return proprietaire;
 	}
 	public void setProprietaire(int proprietaire) {
-		Territoire.proprietaire = proprietaire;
+		this.proprietaire = proprietaire;
 	}
 	public int getId() {
 		return id;
@@ -33,5 +37,152 @@ public class Territoire {
 	public void setId(int id) {
 		this.id = id;
 	}
+	public int getNbrCanon() {
+		return nbrCanon;
+	}
+	public void setNbrCanon(int nbrCanon) {
+		this.nbrCanon = nbrCanon;
+	}
+	public int getNbrCaval() {
+		return nbrCaval;
+	}
+	public void setNbrCaval(int nbrCaval) {
+		this.nbrCaval = nbrCaval;
+	}
+	public int getNbrSoldat() {
+		return nbrSoldat;
+	}
+	public void setNbrSoldat(int nbrSoldat) {
+		this.nbrSoldat = nbrSoldat;
+	}
 	
+	public static void deplacement(int idJoueur, String cartePng)
+	{
+		int idTerrIni=-1;
+		int idTerrCible = -1;
+		int nbrSoldatsDepl=0;
+		int nbrCavalsDepl=0;
+		int nbrCanonsDepl=0;
+		System.out.println("Veuillez sélectionner le territoire d'où vous voulez déplacer des unités");
+		while(idTerrIni == -1)
+		{
+			idTerrIni = Interface.lectureClic(cartePng);
+		}
+		
+		System.out.println("Veuillez sélectionner le nombre d'unités à déplacer puis sélectionnez le territoire où vous souhaitez les envoyer");
+		while(idTerrCible == -1)
+		{
+			nbrSoldatsDepl = Unite.nbrSoldatsDeplacer(cartePng);
+			nbrCavalsDepl = Unite.nbrCavaliersDeplacer(cartePng);
+			nbrCanonsDepl = Unite.nbrCanonsDeplacer(cartePng);
+			
+			idTerrCible = Interface.lectureClic(cartePng);
+		}
+		if(nbrSoldatsDepl==0 && nbrCavalsDepl==0 && nbrCanonsDepl==0)
+		{
+			System.out.println("Vous n'avez sélectionné aucune unité à déplacer");
+		}
+		else if (matriceVoisins[idTerrIni][idTerrCible] != 1)
+		{
+			System.out.println("Veuillez déplacer vos unités case par case");
+		}
+		else
+		{
+			if(nbrSoldatsDepl !=0)
+			{
+				RiskIsep.deplacerSoldat(nbrSoldatsDepl, idTerrIni, idTerrCible, idJoueur);
+			}
+			if(nbrCavalsDepl !=0)
+			{
+				RiskIsep.deplacerCavalier(nbrCavalsDepl, idTerrIni, idTerrCible, idJoueur);
+			}
+			if(nbrCanonsDepl !=0)
+			{
+				RiskIsep.deplacerCanon(nbrCanonsDepl, idTerrIni, idTerrCible, idJoueur);
+			}
+		}
+		
+	}
+	
+	
+
+	public static int territoireDsRegion(int idTerri)
+	{
+		if (idTerri<11)
+		{
+			return idTerri;
+		}
+		else if (idTerri <16)
+		{
+			return idTerri-11;
+		}
+		else if (idTerri <24)
+		{
+			return idTerri-16;
+		}
+		else if (idTerri < 27)
+		{
+			return idTerri-24;
+		}
+		else if (idTerri < 32)
+		{
+			return idTerri-27;
+		}
+		else
+		{
+			return idTerri-32;
+		} 
+	}
+	
+	
+	final static int[][] matriceVoisins= //matrice adjancente déffinissant les pays voisins
+		{
+			{0,1,0,0,0,0,0,0,0,0,0, 1,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{1,0,1,1,0,0,0,0,0,0,0, 0,1,1,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,1,0,1,1,1,0,0,0,0,0, 0,0,1,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,1,1,0,0,1,0,0,0,0,1, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,1,0,0,1,1,0,0,0,0, 0,0,1,0,0, 0,0,1,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,1,1,1,0,1,1,1,1,1, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,1,1,0,1,1,0,0, 0,0,0,0,0, 0,0,1,1,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,1,0,1,0,0, 0,0,0,0,0, 0,0,0,1,0,0,0,0, 0,0,0, 0,0,0,0,0, 1,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,1,1,1,0,1,1, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 1,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,1,0,0,1,0,1, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 1,0,0,0,0,0,0,0,0,0},
+			{0,0,0,1,0,1,0,0,1,1,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+				
+			{1,0,0,0,0,0,0,0,0,0,0,	0,1,0,0,0, 0,0,0,0,0,0,0,0, 0,1,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,1,0,0,0,0,0,0,0,0,0, 1,0,1,1,1, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,1,1,0,1,0,0,0,0,0,0, 0,1,0,1,0, 0,0,1,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,1,1,0,1, 0,0,1,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,1,0,1,0, 1,1,1,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+																																																			
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,1, 0,1,0,0,0,0,0,0, 0,0,0, 1,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,1, 1,0,1,0,0,0,1,1, 0,0,0, 1,0,1,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,1,0,1,0,0,0,0, 0,0,1,1,1, 0,1,0,1,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,1,1,0,0,0, 0,0,0,0,0, 0,0,1,0,1,0,0,1, 0,0,0, 0,0,0,0,0, 1,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,1,0,1,0,1, 0,0,0, 0,0,0,0,0, 1,1,0,0,1,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,1,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,1,0,0,0,0,0,0, 0,0,0, 0,0,1,1,1, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,1,0,1,1,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+																																																		
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,1,1, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 1,0,0,0,0, 0,0,0,0,0,0,0,0, 1,0,1, 0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 1,1,0, 1,0,0,0,0, 0,0,0,0,0,0,0,0,0,0},
+																																																				
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 1,1,0,0,0,0,0,0, 0,0,1, 0,1,1,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 1,0,1,0,0, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,1,0,0,0,0,1,0, 0,0,0, 1,1,0,1,1, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,1,1,0,1, 0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,1,0, 0,0,0, 0,0,1,1,0, 0,0,0,0,0,0,0,0,0,0},
+																																																				
+			{0,0,0,1,0,0,0,1,1,1,0, 0,0,0,0,0, 0,0,0,1,1,0,0,0, 0,0,0, 0,0,0,0,0, 0,1,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,0,0,0, 0,0,0, 0,0,0,0,0, 1,0,1,1,1,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,1,0,0,0,0,0,0,0,1},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,1,0,0,1,1,1,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,1,0,0, 0,0,0, 0,0,0,0,0, 0,1,0,1,0,1,0,1,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,1,1,0,1,1,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,1,0,1,0,1,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,0,0,1,1,1,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,1,0,0,0,0,0,0,1},
+			{0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0,0, 0,0,1,0,0,0,0,0,1,0},			
+		};
 }
